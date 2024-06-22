d9add84dec94c7bd9e1f67b6d9141a98337eff20
Felipe Barreto <felipe.barreto.ifactory@cnpbrasil.com.br>
2024-06-22 17:52:29 -0300

    escrever texto dinamico

---
+++ main.js
@@ -0,0 +1,36 @@
+let j = 0; // controla o tempo que a letra será exibida
+
+function escreverNome(nome) {
+  const container = document.getElementById("nome-container");
+  container.innerText = "";
+
+  const letras = nome.split("");
+
+  while (j < 60) {
+    agendarLetra(letras, letras[j % letras.length], j);
+    incrementarContadores();
+  }
+}
+
+function agendarLetra(letras, letra, tempo) {
+  const container = document.getElementById("nome-container");
+
+  setTimeout(() => {
+    if (container.innerText.length === letras.length) {
+      limpar();
+    }
+
+    console.log("escrevi", container.innerText);
+    container.innerText = container.innerText + letra;
+  }, tempo * 1000);
+}
+
+function limpar() {
+  const container = document.getElementById("nome-container");
+  container.innerText = "";
+  console.log("limpei", container.innerText);
+}
+
+function incrementarContadores() {
+  j = j + 1;
+}
